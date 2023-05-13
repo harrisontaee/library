384e8d9836849abb00726ab6fa9b151a9b7896fa
hpwtaee <hpwtaee@gmail.com>
2023-05-13 09:17:02 +0100

    valid anagram

--- leetcode/two-sum/index.js
+++ leetcode/two-sum/index.js
@@ -7,7 +7,7 @@ export function twoSum(nums, target) { // O(n)
 	const numbers = {};
 	for (let i = 0; i < nums.length; i++) { // O(n)
 		const remainder = target - nums[i];
-		if (remainder in numbers) return [numbers[remainder], i]
+		if (remainder in numbers) return [numbers[remainder], i];
 		else numbers[nums[i]] = i;
 	};
 	return [];
--- leetcode/valid-anagram/index.js
+++ leetcode/valid-anagram/index.js
@@ -4,5 +4,17 @@
  * @return {boolean}
  */
 function isAnagram(s, t) {
-    return false;
+    const letters = {};
+    for (let char of s) {
+        if (char in letters) letters[char]++;
+        else letters[char] = 1;
+    };
+
+    for (let char of t) {
+        if (!(char in letters)) return false;
+        if (letters[char] === 1) delete letters[char];
+        else letters[char]--;
+    }
+
+    return true;
 };
