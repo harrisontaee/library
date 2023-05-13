660ae3bb5966b4166035ecae4f96bac5351efe56
hpwtaee <hpwtaee@gmail.com>
2023-05-13 21:34:52 +0100

    top-k-frequent-elements

--- leetcode/top-k-frequent-elements/index.js
+++ leetcode/top-k-frequent-elements/index.js
@@ -4,5 +4,23 @@
  * @return {number[]}
  */
 function topKFrequent(nums, k) {
-    return [];
+	let freqs = {};
+	for (let num of nums) freqs[num] = (freqs[num] || 0) + 1;
+	let array = [];
+	for (let num in freqs) {
+		let freq = freqs[num];
+		array[freq] = (array[freq] || new Set()).add(num);
+	};
+
+	let result = [];
+	for (let i = array.length - 1; i >= 0; i--) {
+		if (!array[i]) continue;
+		result.push(...array[i]);
+		if (result.length >= k) {
+			result = result.slice(0, k);
+			break;
+		}
+	};
+
+	return result;
 };
