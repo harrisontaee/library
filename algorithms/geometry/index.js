ce1da28ab820220ad8f9288dde94aa609bf0ec86
hpwtaee <hpwtaee@gmail.com>
2023-05-20 16:15:22 +0100

    proper segmentat intersection algorithm

--- algorithms/geometry/index.js
+++ algorithms/geometry/index.js
@@ -5,28 +5,57 @@
  * @property {number} y
  */
 /**
- * Segments Intersect
- * @description Given segments s1(p1, p2) and s2(p3, p4), determine if they intersect.
- * @param {Point} p1 segment 1 point 1
- * @param {Point} p2 segment 1 point 2
- * @param {Point} p3 segment 2 point 1
- * @param {Point} p4 segment 2 point 2
- * @return {boolean}
+ * Get Intersection
+ * @description Given segments (a, b) and (c, d), get the intersection.
+ * @param {Point} a segment 1 point 1
+ * @param {Point} b segment 1 point 2
+ * @param {Point} c segment 2 point 1
+ * @param {Point} d segment 2 point 2
+ * @return {Point | null}
  */
-export function segmentsIntersect(p1, p2, p3, p4) {
-	let x1 = p1.x,
-		 y1 = p1.y,
-		 x2 = p2.x,
-		 y2 = p2.y,
-		 x3 = p3.x,
-		 y3 = p3.y,
-		 x4 = p4.x,
-		 y4 = p4.y;
-	// if two largest x values belong to the same line, don't intersect
-	if (Math.min(x1, x2) > Math.max(x3, x4)) return false;
-	if (Math.min(x3, x4) > Math.max(x1, x2)) return false;
-	// if two largest y values belong to the same line, don't intersect
-	if (Math.min(y1, y2) > Math.max(y3, y4)) return false;
-	if (Math.min(y3, y4) > Math.max(y1, y2)) return false;
-	return true;
+export function getSegmentIntersection(a, b, c, d) {
+	// ix: ax + (bx - ax)t = cx + (dx - cx)u
+	// iy: ay + (by - ay)t = cy + (dy - cy)u
+
+	// [ix: -cx, iy: -cy]
+	// ix: (ax - cx) + (bx - ax)t = (dx - cx)u
+	// iy: (ay - cy) + (by - ay)t = (dy - cy)u
+
+	// [iy: *(dx - cx)]
+	// iy: (ay - cy)(dy - cy) + (by - ay)(dy - cy)t = (dy - cy)(dx - cx)u
+
+	// [iy: sub (dx - cx)u]
+	// iy: (ay - cy)(dy - cy) + (by - ay)(dy - cy)t = (dy - cy)(ax - cx) + (dy - cy)(bx - ax)t;
+	
+	// [iy: - (ay - cy)(dy - cy) - (dy - cy)(bx - ax)t]
+	// iy: t((by - ay)(dy - cy) - (dy - cy)(bx - ax)) = (dy - cy)(ax - cx) - (ay - cy)(dy - cy)
+
+	// [iy: /((by - ay)(dy - cy) - (dy - cy)(bx - ax))]
+	// iy: t = ((dy - cy)(ax - cx) - (ay - cy)(dy - cy)) / ((by - ay)(dy - cy) - (dy - cy)(bx - ax))
+
+	const upper = ((d.y - c.y) * (a.x - c.x) - (a.y - c.y) * (d.y - c.y));
+	const lower = ((b.y - a.y) * (d.y - c.y) - (d.y - c.y) * (b.x - a.x));
+
+	if (lower === 0) return null;
+
+	const t = upper / lower;
+	if (t < 0 || t > 1) return null;
+	
+	return {
+		x: interpolate(a.x, b.x, t),
+		y: interpolate(a.y, b.y, t)
+	};
+};
+
+
+
+/**
+ * Interpolate
+ * @param {number} a
+ * @param {number} b
+ * @param {number} t 0 >= t <= 1
+ * @return {number}
+ */
+function interpolate(a, b, t) {
+	return a + ((b - a) * t);
 };
