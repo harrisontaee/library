b17d82883f07b14d629684cb8a301d991704e2b3
hpwtaee <hpwtaee@gmail.com>
2023-05-24 17:52:05 +0100

    updated geometry

--- algorithms/geometry/index.ts
+++ algorithms/geometry/index.ts
@@ -4,7 +4,61 @@ export interface Point {
 };
 
 
-export const interpolate = (a: number, b: number, t: number): number => a + ((b - a) * t);
+
+
+
+/**
+ * @param {number} x center of the circle
+ * @param {number} y center of the circle
+ */
+export interface Circle {
+	x: number;
+	y: number;
+	r: number;
+};
+
+
+
+
+/**
+ * @param {number} x bottom left corner of the rectangle
+ * @param {number} y bottom left corner of the rectangle
+ */
+export interface Rectangle {
+	x: number;
+	y: number;
+	w: number;
+	h: number;
+};
+
+
+
+
+
+export const interpolate = (a: number, b: number, t: number): number => {
+	return a + ((b - a) * t);
+};
+
+
+
+
+
+export const distanceBetweenPoints = (a: Point, b: Point): number => {
+	return Math.sqrt((a.x - b.x) ** 2 + (a.y - b.y) ** 2);
+};
+
+
+
+
+
+export const pointInsideCircle = (point: Point, circle: Circle): boolean => {
+	return distanceBetweenPoints(point, circle) <= circle.r;
+};
+
+
+
+
+
 export const getSegmentsIntersection = (a: Point, b: Point, c: Point, d: Point) => {
 	const tNumerator = ((d.x - c.x) * (a.y - c.y)) - ((d.y - c.y) * (a.x - c.x));
 	const uNumerator = ((c.y - a.y) * (a.x - b.x)) - ((c.x - a.x) * (a.y - b.y));
