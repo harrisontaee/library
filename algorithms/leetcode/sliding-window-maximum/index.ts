506f9e22e20173916670e818aa09789fb064c28d
hpwtaee <hpwtaee@gmail.com>
2023-05-31 20:36:29 +0100

    sliding-window-maximum

--- algorithms/leetcode/sliding-window-maximum/index.ts
+++ algorithms/leetcode/sliding-window-maximum/index.ts
@@ -1,1 +1,22 @@
-export const slidingWindowMaximum = () => {};
+export const slidingWindowMaximum = (nums: number[], k: number): number[] => {
+	const maxes: number[] = [];
+	const queue: number[] = [];
+
+	let l = 0, r = 0;
+
+	while (r < nums.length) {
+
+		while (!!queue.length && nums[queue[queue.length - 1]] < nums[r]) queue.pop();
+		queue.push(r);
+
+		if (l > queue[0]) queue.shift();
+		if (r >= k - 1) {
+			maxes.push(nums[queue[0]]);
+			l++;
+		};
+
+		r++;
+	};
+
+	return maxes;
+};
