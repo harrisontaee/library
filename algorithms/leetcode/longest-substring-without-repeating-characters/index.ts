7a51120bc3624d3dba81e9ea69d36149f5ecd614
hpwtaee <hpwtaee@gmail.com>
2023-05-31 09:24:12 +0100

    longest-substring-without-repeating-characters

--- algorithms/leetcode/longest-substring-without-repeating-characters/index.ts
+++ algorithms/leetcode/longest-substring-without-repeating-characters/index.ts
@@ -1,1 +1,13 @@
-export const longestSubstringWithoutRepeatingCharacters = () => {};
+export const longestSubstringWithoutRepeatingCharacters = (s: string): number => {
+	let chars = new Set<string>([]);
+	let max = 0;
+
+	for (let char of s) {
+		if (chars.has(char)) {
+			max = Math.max(max, chars.size);
+			chars = new Set([char]);
+		} else chars.add(char);
+	};
+
+	return Math.max(max, chars.size);
+};
