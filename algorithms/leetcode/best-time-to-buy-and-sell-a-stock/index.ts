7e70db6bf9c3fe8403216762e45524b8552f838f
hpwtaee <hpwtaee@gmail.com>
2023-05-31 09:17:18 +0100

    best-time-to-buy-and-sell-a-stock

--- algorithms/leetcode/best-time-to-buy-and-sell-a-stock/index.ts
+++ algorithms/leetcode/best-time-to-buy-and-sell-a-stock/index.ts
@@ -1,1 +1,11 @@
-export const bestTimeToBuyAndSellAStock = () => {};
+export const bestTimeToBuyAndSellAStock = (prices: number[]) => {
+	let profit = 0;
+	let min = Infinity;
+
+	for (let price of prices) {
+		if (price - min > profit) profit = price - min;
+		if (price < min) min = price;
+	};
+
+	return profit;
+};
